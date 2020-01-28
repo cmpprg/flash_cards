61ce139727b143089f69b767dca1cb0852582be2
Ryan Camp <cmpprg@icloud.com>
2020-01-28 16:39:34 -0700

    Create test for cards attribute starting empty

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,31 +6,24 @@ require "./lib/deck"
 class TestDeck < Minitest::Test
 
   def test_that_object_is_instance_of_deck_class
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+
+    deck = Deck.new()
 
     assert_instance_of Deck, deck
   end
+
+  def test_that_cards_attribute_starts_empty
+    
+    deck = Deck.new()
+
+    assert_equal [], deck.cards
+  end
 end
 
-
-# pry(main)> card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
-#
-# pry(main)> card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
-#
-# pry(main)> card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
-#
-# pry(main)> cards = [card_1, card_2, card_3]
-#
-# pry(main)> deck = Deck.new(cards)
-# #=> #<Deck:0x00007fa160a38ed8...>
-#
+#card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# cards = [card_1, card_2, card_3]
 # pry(main)> deck.cards
 # #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
 #
