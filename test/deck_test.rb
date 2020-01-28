f5e6cd8442094c5eda22f07b5b652d56c2bdb7bd
Ryan Camp <cmpprg@icloud.com>
2020-01-28 16:59:42 -0700

    Create test to check that cards holds card objects

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -13,17 +13,27 @@ class TestDeck < Minitest::Test
   end
 
   def test_that_cards_attribute_starts_empty
-    
+
     deck = Deck.new()
 
     assert_equal [], deck.cards
   end
+
+  def test_cards_can_holds_card_objects
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.cards.count
+    assert_kind_of Card, deck.cards[0]
+    assert_kind_of Card, deck.cards[1]
+    assert_kind_of Card, deck.cards[2]
+    
+  end
 end
 
-#card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-# cards = [card_1, card_2, card_3]
 # pry(main)> deck.cards
 # #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
 #
