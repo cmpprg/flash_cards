73c63411c04246b01f1941fc36376f345989edcb
Ryan Camp <cmpprg@icloud.com>
2020-01-27 20:21:16 -0700

    Develop minitest for turn class

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,5 +4,48 @@ require_relative "./lib/turn.rb"
 require_realtive "./lib/card.rb"
 
 class TestTurn < Minitest::TestTurn
-  
+
+  def test_that_object_exists
+    turn = Turn.new
+    assert_instance_of Card, card
+  end
+
+  def test_that_it_accepts_arguments
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", turn.card.answer
+  end
+
+  def test_that_correct_variable_defaults_to_false
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    refute turn.correct?
+  end
+
+  def test_that_it_tracks_correct_and_incorrect_answers
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert turn.correct?
+
+    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn2 = Turn.new("Saturn", card)
+
+    refute turn.correct?
+  end
+
+  def test_that_it_can_provide_feedback_for_correct_or_incorrect_answers
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!"
+
+    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn2 = Turn.new("Saturn", card)
+
+    assert_equal "Incorrect!"
+  end
 end
