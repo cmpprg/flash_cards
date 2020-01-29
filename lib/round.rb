ac9f0b786d46b3b5a7aadc2ee89b6cf8b2181b14
Ryan Camp <cmpprg@icloud.com>
2020-01-29 16:09:11 -0700

    Pass test for take turn test

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,16 @@
 class Round
   attr_reader :deck, :turns
+
   def initialize deck
     @deck = deck
     @turns = []
+  end
+
+  def current_card
+    @deck.first
+  end
+
+  def take_turn(guess)
+    Turn.new(guess, current_card())
   end
 end
