b54f3df6b15a8cb6f0b30a00f9df0cd67d6d1264
Ryan Camp <cmpprg@icloud.com>
2020-01-29 15:36:52 -0700

    Create test the check turns variable

--- test/round_test.rb
+++ test/round_test.rb
@@ -27,12 +27,19 @@ class TestRound < Minitest::Test
     assert_equal deck, round.deck
   end
 
+  def test_turns_variable_exists_and_is_empty
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = [card1, card2, card3]
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+  end
+
 end
 
 
-# pry(main)> round.deck
-# #=> #<Deck:0x00007fa160a38ed8...>
-#
 # pry(main)> round.turns
 # #=> []
 #
