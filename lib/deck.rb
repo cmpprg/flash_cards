06ad86f768c049a99f3f49b52cda7e23d928078e
Ryan Camp <cmpprg@icloud.com>
2020-01-28 17:36:08 -0700

    Construct cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,14 @@ class Deck
     cards.count
   end
 
+  def cards_in_category(category)
+    category_array = []
+    cards.each do |card|
+      if card.category == category
+        category_array << card
+      end
+    end
+    category_array
+  end
+
 end
