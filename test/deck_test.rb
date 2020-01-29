61e2c3b95dfb350b675d5b61d3043f1c2fdc879c
Ryan Camp <cmpprg@icloud.com>
2020-01-28 17:05:38 -0700

    Create test to count cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -30,13 +30,19 @@ class TestDeck < Minitest::Test
     assert_kind_of Card, deck.cards[0]
     assert_kind_of Card, deck.cards[1]
     assert_kind_of Card, deck.cards[2]
-    
+  end
+
+  def test_count_method_counts_number_of_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
   end
 end
 
-# pry(main)> deck.cards
-# #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
-#
 # pry(main)> deck.count
 # #=> 3
 #
