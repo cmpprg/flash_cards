bbe8ed54b261d88793c54dbcc3e182315e93d687
Ryan Camp <cmpprg@icloud.com>
2020-01-29 19:38:13 -0700

    Create test to check that number_correct does not count incorrect answers

--- test/round_test.rb
+++ test/round_test.rb
@@ -94,13 +94,22 @@ class TestRound < Minitest::Test
     assert_equal card2, round.current_card
   end
 
+  def test_that_number_correct_does_not_count_incorrect
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = [card1, card2, card3]
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    next_turn = round.take_turn("Venus")
+
+    assert_equal 2, round.turns.count
+    assert_equal "Incorrect.", round.turns.last.feedback
+    assert_equal 1, round.number_correct
+  end
 
 end
 
-
-# pry(main)> round.current_card
-# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
-#
 # pry(main)> round.take_turn("Venus")
 # #=> #<Turn:0x00007f972a215b38...>
 #
