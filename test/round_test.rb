69cb0536f7b0cf4222b0f7649645437f28b8d6de
Ryan Camp <cmpprg@icloud.com>
2020-01-29 19:20:36 -0700

    Create test to check correct answers counted

--- test/round_test.rb
+++ test/round_test.rb
@@ -70,17 +70,22 @@ class TestRound < Minitest::Test
 
     assert_includes round.turns, new_turn
     assert_equal new_turn, round.turns[0]
+  end
 
+  def test_variable_that_counts_number_correct
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = [card1, card2, card3]
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal 1, round.number_correct
 
   end
 end
 
-# pry(main)> round.turns
-# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
-#
-# pry(main)> round.number_correct
-# #=> 1
-#
+
 # pry(main)> round.current_card
 # #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
 #
