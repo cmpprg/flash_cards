dc84a1ea5c7a04736b33ec536d488e4e680c93da
Ryan Camp <cmpprg@icloud.com>
2020-01-29 19:21:48 -0700

    Complete counting corrects guesses test

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,11 @@
 class Round
   attr_reader :deck, :turns
+  attr_accessor :number_correct
 
   def initialize deck
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
@@ -13,6 +15,7 @@ class Round
   def take_turn(guess)
     turn = Turn.new(guess, current_card())
     @turns << turn
+    @number_correct += 1 if turn.correct? == true
     turn
   end
 end
