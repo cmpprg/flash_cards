63fad9c0cde7ba1a6b90b7d6a21301e3f05bfe7e
Ryan Camp <cmpprg@icloud.com>
2020-01-31 12:31:22 -0700

    Modify instance variables in test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,16 +1,17 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/card"
+require "./lib/card_generator"
 
 
 class CardGeneratorTest < Minitest::Test
 
   def setup
-    filename = "./data/cards.txt"
-    generator = CardGenerator.new(filename)
+    @filename = "./data/cards.txt"
+    @generator = CardGenerator.new(@filename)
   end
 
   def test_it_exists
-    assert_instance_of CardGenerator, generator
+    assert_instance_of CardGenerator, @generator
   end
 end
