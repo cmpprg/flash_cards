9ce1fda2feee2d771ecf0b3c15dab87e579a854d
Ryan Camp <cmpprg@icloud.com>
2020-02-01 06:56:24 -0700

    Refactor test to include blank text file

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -7,23 +7,28 @@ require "./lib/card_generator"
 class CardGeneratorTest < Minitest::Test
 
   def setup
-    @filename = "./data/cards.txt"
-    @generator = CardGenerator.new(@filename)
+    @blank_file = "./data/blank.txt"
+    @cards_file = "./data/cards.txt"
   end
 
   def test_it_exists
-    assert_instance_of CardGenerator, @generator
+    generator = CardGenerator.new(@blank_file)
+
+    assert_instance_of CardGenerator, generator
   end
 
   def test_instance_variables
-    assert_equal @filename, @generator.filename
-    assert_equal [], @generator.cards
+    generator = CardGenerator.new(@blank_file)
+
+    assert_equal @blank_file, generator.filename
+    assert_equal [], generator.cards
   end
 
   def test_generate_cards_method
-    @generator.generate_cards
-    assert_equal 3, @generator.cards.length
-    assert_instance_of Card, @generator.cards[2]
+    generator = CardGenerator.new(@cards_file)
+
+    assert_equal 3, generator.cards.length
+    assert_instance_of Card, generator.cards[2]
   end
 
 
