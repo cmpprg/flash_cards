3e2a795e624c1b183807a750123d711f2a61ae50
Ryan Camp <cmpprg@icloud.com>
2020-02-01 07:05:34 -0700

    Make generate cards method & implement in initialize

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,6 +3,24 @@ class CardGenerator
   def initialize(filename)
     @filename = filename
     @cards = []
+    generate_cards
+  end
+
+# open file for reading,
+# start iteration:
+  # capture single line, transform string to array,
+  # create new card, populate @cards,
+#end iteration
+# close file.
+  def generate_cards
+    target_file = File.open(@filename)
+    until target_file.eof?
+      line = target_file.readline.chomp
+      line_split = line.split(",")
+      card = Card.new(line_split[0], line_split[1], line_split[2])
+      @cards << card
+    end
+    target_file.close
   end
 
 end
