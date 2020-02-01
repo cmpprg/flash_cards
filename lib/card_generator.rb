3ae12076e359856209533232b37848cd1087bf19
Ryan Camp <cmpprg@icloud.com>
2020-02-01 08:47:05 -0700

    Rename some elements and add feature that removes white space from data

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -9,15 +9,17 @@ class CardGenerator
 # open file for reading,
 # start iteration:
   # capture single line, transform string to array,
+  # strip each element to ensure no white space on front or back.
   # create new card, populate @cards,
 #end iteration
 # close file.
   def generate_cards
     target_file = File.open(@filename)
     until target_file.eof?
-      line = target_file.readline.chomp
-      line_split = line.split(",")
-      card = Card.new(line_split[0], line_split[1], line_split[2])
+      single_line = target_file.readline.chomp
+      data_set = single_line.split(",")
+      stripped_data = data_set.map{|datum| datum.strip}
+      card = Card.new(stripped_data[0], stripped_data[1], stripped_data[2])
       @cards << card
     end
     target_file.close
